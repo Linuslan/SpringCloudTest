b7ec1c9c10457e21f41696826f220e736c515385
Linuslan <linuslan@sina.cn>
2018-04-26 15:29:40 +0800

    1

--- service-ribbon/src/main/java/com/linuslan/serviceribbon/ServiceRibbonApplication.java
+++ service-ribbon/src/main/java/com/linuslan/serviceribbon/ServiceRibbonApplication.java
@@ -1,10 +1,13 @@
 package com.linuslan.serviceribbon;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +20,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @ComponentScan(basePackages = "com.linuslan")
 @EnableHystrix
+@EnableHystrixDashboard
 public class ServiceRibbonApplication {
 
     public static void main(String[] args) {
@@ -27,5 +31,14 @@ public class ServiceRibbonApplication {
     @LoadBalanced
     RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
--- service-zuul/src/main/java/com/linuslan/servicezuul/ServiceZuulApplication.java
+++ service-zuul/src/main/java/com/linuslan/servicezuul/ServiceZuulApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
+@ComponentScan(basePackages = "com.linuslan")
 public class ServiceZuulApplication {
 
     public static void main(String[] args) {
