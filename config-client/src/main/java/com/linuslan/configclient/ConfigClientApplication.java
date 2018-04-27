bfdddf6f02ac96bbc30ebe047efc1666b09552fd
Linuslan <linuslan@sina.cn>
2018-04-27 10:05:34 +0800

    111

--- config-client/src/main/java/com/linuslan/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/linuslan/configclient/ConfigClientApplication.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @EnableEurekaClient
+@RefreshScope
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
